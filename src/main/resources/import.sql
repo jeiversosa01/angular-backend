6879d37d8283be22288a99a7766d05ef96e657de
jeiversosa01 <jeiversosa01@gmail.com>
2022-11-06 15:50:40 -0500

    Datos de ejemplo utilizados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Diosemel', 'Sosa', 'diosemelsosa@gmail.com', '1963-08-18');
 INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Zenaida', 'Alvarez', 'zenaidaalvarez@gmail.com', '1967-06-28');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Jefferson', 'Sosa', 'jeffe-87@hotmail.com', '1987-01-04');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Breiner', 'Sosa', 'bresosal@hotmail.com', '1989-01-04');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Jeiver', 'Sosa', 'jeiversosa01@gmail.com', '1999-10-20');
+INSERT INTO clientes (nombre, apellido, email, fecha) VALUES('Keidy', 'Sosa', 'keidysosa17@gmail.com', '2007-12-17');
